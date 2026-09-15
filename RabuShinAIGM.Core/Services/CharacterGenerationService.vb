bf19fc6527553a59f80a05b9ebb9ddf1c8b46017
DailyOpsReportBot <299204837+DailyOpsReportBot@users.noreply.github.com>
2026-09-14 20:42:59 -0400

    Fix True Neutral character creation validation

--- RabuShinAIGM.Core/Services/CharacterGenerationService.vb
+++ RabuShinAIGM.Core/Services/CharacterGenerationService.vb
@@ -23,7 +23,7 @@ Public Class CharacterGenerationService
     }
 
     Public Shared ReadOnly Alignments As String() = {
-        "Lawful Good", "Neutral Good", "Chaotic Good", "Lawful Neutral", "Neutral", "Chaotic Neutral",
+        "Lawful Good", "Neutral Good", "Chaotic Good", "Lawful Neutral", "True Neutral", "Chaotic Neutral",
         "Lawful Evil", "Neutral Evil", "Chaotic Evil"
     }
 
